70a7900ec4fc19fab9c26100b7584c474ab12d26
JLopes2021 <jeffinho.je8@gmail.com>
2023-11-20 22:03:26 -0300

    modelo-inicial

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistema de Estoque - Mercado de Horticultura</title>
+    <!-- Adicione links para os arquivos CSS do Bootstrap se necessário -->
+    <!-- <link rel="stylesheet" href="caminho/para/bootstrap/css/bootstrap.min.css"> -->
+</head>
+<body>
+
+    <div class="container">
+        <header>
+            <h1>Sistema de Estoque</h1>
+            <p>Bem-vindo ao sistema de gestão de estoque do Mercado de Horticultura.</p>
+        </header>
+
+        <section>
+            <h2>O Projeto</h2>
+            <p>
+                Este projeto visa fornecer uma solução eficiente para o gerenciamento de estoque em um mercado de horticultura.
+                Através deste sistema, você poderá visualizar informações sobre produtos, gerenciar o estoque, realizar vendas
+                e manter um registro de clientes.
+            </p>
+        </section>
+
+        <section>
+            <h2>Funcionalidades Principais</h2>
+            <ul>
+                <li>Visualização de produtos e suas informações essenciais.</li>
+                <li>Gestão de estoque, permitindo atualizações e rastreamento de alterações.</li>
+                <li>Registro de vendas, incluindo detalhes sobre os produtos vendidos e a data da transação.</li>
+                <li>Cadastro e manutenção de informações de clientes.</li>
+            </ul>
+        </section>
+
+        <footer>
+            <p>&copy; <?php echo date("Y"); ?> Sistema de Estoque - Mercado de Horticultura</p>
+        </footer>
+    </div>
+
+    <!-- Adicione links para os arquivos JavaScript do Bootstrap e outros scripts, se necessário -->
+    <!-- <script src="caminho/para/bootstrap/js/bootstrap.min.js"></script> -->
+
+</body>
+</html>
